686245dbc771dcacf1438e78a02b74729784ae1c
Martin Emanuel <martinemanuel93@gmail.com>
2024-06-12 17:35:07 -0300

    integrate github auth provider

--- routes.js
+++ routes.js
@@ -7,8 +7,20 @@ module.exports = function (app, myDataBase) {
     res.render(process.cwd() + "./views/pug/index", {
       title: "Connected to Database",
       message: "Please login",
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true,
     });
   });
+  app.route("/auth/github").get(passport.authenticate("github"));
+  app
+    .route("/auth/github/callback")
+    .get(
+      passport.authenticate("github", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
   app.route("/login").post(
     passport.authenticate("local", {
       failureRedirect: "/",
